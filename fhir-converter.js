8826e7d974a528095fca7260198293ef865681dc
Nikolaos Alexandros Kaloumenos <nakaloumenos@gmail.com>
2017-08-11 12:33:35 +0100

    Start building the converter

---
+++ fhir-converter.js
@@ -0,0 +1,10 @@
+var Converter = function (mapConf) {
+	this.m = require('./map config/'+mapConf);
+
+	this.convert = function (data) {
+		var mapped = m.map_existing_keys(data);
+		return mapped;
+	}
+};
+
+module.exports = Converter;
